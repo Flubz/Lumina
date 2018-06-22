8442d084a5a98661d92e527b7e6c4da44bf9a35f
Unknown <fjgamerz@gmail.com>
2018-06-23 00:19:27 +0100

    Added Player Movement

---
+++ Assets/Scripts/Extensions/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+	public static void RotateTowardsVector (this Transform trans_, Vector2 vel_, float rotSpeed_ = 4.0f, float angleOffset_ = 0.0f)
+	{
+		float angle = Mathf.Atan2 (vel_.y, vel_.x) * Mathf.Rad2Deg;
+		trans_.rotation = Quaternion.Lerp (trans_.rotation, Quaternion.AngleAxis (angle + angleOffset_, Vector3.up * -1), Time.deltaTime * rotSpeed_);
+	}
+
+	public static T RandomFromList<T> (this List<T> list_)
+	{
+		return list_[UnityEngine.Random.Range (0, list_.Count)];
+	}
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	[SerializeField] Rigidbody _rb;
+	[SerializeField] float _moveSpeed;
+	[SerializeField] float _rotSpeed;
+	[SerializeField] float _rotOffset;
+
+	private void FixedUpdate ()
+	{
+		if (GetRotationVector ().magnitude > 0.4f)
+		{
+			transform.RotateTowardsVector (GetRotationVector (), _rotSpeed, _rotOffset);
+			_rb.velocity = (transform.forward * Time.deltaTime * _moveSpeed);
+		}
+	}
+
+	Vector3 GetRotationVector ()
+	{
+		return new Vector3 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"), 0);
+	}
+
+}
