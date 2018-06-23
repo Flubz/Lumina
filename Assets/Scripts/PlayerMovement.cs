cab8de820baedbd3cb7e6b279cf55556e671e29d
Unknown <fjgamerz@gmail.com>
2018-06-23 19:01:42 +0100

    Added teleportation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -10,21 +11,68 @@ public class PlayerMovement : MonoBehaviour
 	[SerializeField] float _maxVelocity;
 	[SerializeField] float _rotSpeed;
 	[SerializeField] float _rotOffset;
+	[Header ("Dash Settings")]
+	[SerializeField] Transform _orb;
+	[SerializeField] float _maxDashDistance;
+	[SerializeField] float _orbMoveSpeed;
+	[SerializeField] Ease _dashEase;
+
+	bool _movementLocked;
+
+	void Start ()
+	{
+		_orb.gameObject.SetActive (false);
+	}
 
 	private void FixedUpdate ()
 	{
+		if (_movementLocked) return;
 		if (GetRotationVector ().magnitude > 0.4f)
 		{
 			transform.RotateTowardsVector (GetRotationVector (), _rotSpeed, _rotOffset);
 			_rb.AddForce (transform.forward * _moveSpeed, ForceMode.Force);
 			if (_rb.velocity.magnitude >= _maxVelocity) _rb.velocity = _rb.velocity * _maxVelocity;
-			Debug.Log (_rb.velocity.magnitude);
 		}
 	}
 
 	Vector3 GetRotationVector ()
 	{
 		return new Vector3 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"), 0);
+	}
+
+	void Update ()
+	{
+		CheckForDash ();
+	}
+
+	void CheckForDash ()
+	{
+		if (Input.GetButtonDown ("Jump"))
+			LaunchOrb ();
+
+		if (Input.GetButtonUp ("Jump"))
+			TeleportToOrb ();
+
+	}
+
+	void LaunchOrb ()
+	{
+		_orb.gameObject.SetActive (true);
+		_movementLocked = true;
+		_orb.position = transform.position;
+
+		Vector3 telePos = transform.position + transform.forward * _maxDashDistance;
+
+		_orb.DOMoveX (telePos.x, _orbMoveSpeed).SetEase (_dashEase);
+		_orb.DOMoveZ (telePos.z, _orbMoveSpeed).SetEase (_dashEase);
+	}
+
+	void TeleportToOrb ()
+	{
+		transform.position = _orb.position;
+
+		_movementLocked = false;
+		_orb.gameObject.SetActive (false);
 	}
 
 }
